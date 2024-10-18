5f16b5d8bffaaf421c2cc6e511da5977fab75eef
Rishita Bangar <ribangar@bestpeers.in>
2024-10-18 12:04:07 +0530

    counterapp created

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [state,setState]=useState(0);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    // <div style={{}}>
+    <div style={{width:'100%',backgroundColor:'pink',marginLeft: '70%'}}>
+
+      <h1 style={{ textAlign: 'center' }}>Counter app</h1><br/>
+      <h3 style={{ textAlign: 'center' }}>{state}</h3>
+      <button onClick={()=>{
+
+          if(state>=0){
+            setState(state+1);
+          }
+          else
+          {
+            setState(state-1);
+          }
+                }
+      }>
+        increase</button>
+        <button onClick={()=>{
+          setState(0);
+        }}>clear</button>
+      <button onClick={()=>{
+        if(state>0)
+        {
+          setState(state-1);
+        }
+
+      }}>decrease</button>
+    </div>
   )
 }
 
